08f74bfe39a081ae9e1c0ffccddd865e3ab97076
Adrian Hatfield <99419677+adrianwhatfield@users.noreply.github.com>
2022-12-02 10:53:33 +0000

    Complete 02 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newString = ""
 
+    if (num < 0) {
+        return "ERROR";
+    }
+    while (num > 0) {
+        newString += string;
+        num--;
+    }
+    return newString;
 };
+
+const number = Math.floor(Math.random() * 1000)
+
+repeatString("hey", 3);
+repeatString("hey", 10);
+repeatString("hey", 1);
+repeatString("hey", 0);
+repeatString("hey", -1);
+repeatString("hey", number);
+repeatString("", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
