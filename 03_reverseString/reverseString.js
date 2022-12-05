a1aa738752c90abc0bb2ab4f5c57841a485d7929
Adrian Hatfield <99419677+adrianwhatfield@users.noreply.github.com>
2022-12-05 15:58:25 +0000

    Complete 03 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split("")
+    let newArr = arr.reverse()
+    return newArr.join("")
 };
+
+reverseString("hello")
+reverseString("hello there")
+reverseString("123! abc!")
+reverseString("")
 
 // Do not edit below this line
 module.exports = reverseString;
