d43ebccf484329e3a8b62a6729936096800f59eb
Adrian Hatfield <99419677+adrianwhatfield@users.noreply.github.com>
2023-01-04 16:15:02 +0000

    Complete 07 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let result = (num - 32) * (5 / 9);
+  result = parseFloat(((result * 10) / 10).toFixed(1));
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let result = num * (9 / 5) + 32;
+  result = parseFloat(((result * 10) / 10).toFixed(1));
+  return result;
 };
+
+convertToCelsius(32);
+convertToCelsius(100);
+convertToCelsius(-100);
+convertToFahrenheit(0);
+convertToFahrenheit(73.2);
+convertToFahrenheit(-10);
 
 // Do not edit below this line
 module.exports = {
