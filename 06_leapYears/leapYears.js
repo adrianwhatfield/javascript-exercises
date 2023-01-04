bdbcd84760d1d8209808f9d14d149aadc96fe63a
Adrian Hatfield <99419677+adrianwhatfield@users.noreply.github.com>
2023-01-04 15:36:53 +0000

    Complete 06 leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,23 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let four = year/4;
+    let fourHundred = year/400;
+    let oneHundred = year/100;
 
+    if (Number.isInteger(four) == true && Number.isInteger(oneHundred) == false) {
+        return true;
+    } else if (Number.isInteger(fourHundred) == true) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+leapYears(1996);
+leapYears(1997);
+leapYears(34992);
+leapYears(1900);
+leapYears(1600);
+leapYears(700);
 
 // Do not edit below this line
 module.exports = leapYears;
