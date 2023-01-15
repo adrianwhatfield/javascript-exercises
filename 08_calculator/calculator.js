2d007298e9014af30b9534f05b615ea725de8f47
Adrian Hatfield <99419677+adrianwhatfield@users.noreply.github.com>
2023-01-15 16:03:28 +0000

    Complete 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+  if (num.length === 0) {
+    return 0;
+  } else {
+    return num.reduce(sumArr);
+
+    function sumArr(total, num) {
+      return total + num;
+    }
+  }
 };
 
-const multiply = function() {
+const multiply = function(num) {
+  return num.reduce(multiplyArr);
 
+  function multiplyArr(total, num) {
+    return total * num;
+  }
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+
+	if (a == 0) {
+    return 1;
+  } else {
+    for (let i = 2; i <= a; i++) {
+      result = result * i;
+    };
+  };
+
+  return result;
 };
 
 // Do not edit below this line
