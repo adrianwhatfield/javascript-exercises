c4b170bf2cc2bd3e2f6eeb2af617e746b6e29c8c
Adrian Hatfield <adrianhatfield23@gmail.com>
2023-01-17 13:41:48 +0000

    Complete 11 getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    let list = [];
 
+    for (let i = 0; i < arr.length; i ++) {
+        const book = arr[i];
+        let title = book.title;
+
+        list.push(title);
+    };
+
+    return list;
 };
 
 // Do not edit below this line
