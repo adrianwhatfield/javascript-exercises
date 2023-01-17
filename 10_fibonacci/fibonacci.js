6649ba9f9628cbfe6b6f16f98fcce9e923dab338
Adrian Hatfield <adrianhatfield23@gmail.com>
2023-01-17 10:00:42 +0000

    Complete 10 Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(place) {
+    if (place < 0) {
+        return "OOPS";
+    };
 
+    place = parseInt(place);
+    
+    let a = 0;
+    let b = 1;
+    let next = 1;
+
+    for (let i = 1; i < place; i++) {
+        next = a + b;
+        a = b;
+        b = next;
+    };
+
+    return next;
 };
 
 // Do not edit below this line
