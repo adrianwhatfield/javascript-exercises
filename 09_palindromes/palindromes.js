5e81fe727c9dded778b41d113dbec64d427f3218
Adrian Hatfield <adrianhatfield23@gmail.com>
2023-01-17 09:39:34 +0000

    Complete 09 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,34 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    // Make the whole string just lower case letters
+    text = text.replaceAll("!", "");
+    text = text.replaceAll(".", "");
+    text = text.replaceAll(",", "");
+    text = text.replaceAll(" ", "");
+    text = text.toLowerCase();
 
+
+    // Get start and end of text
+
+    let length = text.length;
+    let part = ((length - 1) / 2) + 1;
+
+    let start = text.slice(0, part);
+    let end = text.slice(-part);
+
+    // Convert end to an array, reverse, then join
+
+    let endArrBack = end.split("");
+    let endArr = endArrBack.reverse();
+
+    end  = endArr.join("");
+
+    // Compare the start and end
+
+    if (start == end) {
+        return true;
+    } else {
+        return false;
+    };
 };
 
 // Do not edit below this line
