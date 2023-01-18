9339699469fc302d77ff42dbe992e4f0ae1961b4
Adrian Hatfield <adrianhatfield23@gmail.com>
2023-01-18 15:18:16 +0000

    Complete 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldestPerson = people.reduce(compareAges);
 
+    function compareAges(oldest, current) {
+        let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+
+        if (currentAge > oldestAge) {
+            return oldest = current;
+        }
+
+        return oldest;
+    };
+
+    function getAge(birth, death) {
+        if (!death) {
+            death = 2022;
+        };
+
+        return death - birth;
+    };
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
